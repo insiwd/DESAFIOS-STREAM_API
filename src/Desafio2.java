aa38bee2b9580127b53ed4d97a4f5243fb589f6a
ruan <ruan.cavalcante505@gmail.com>
2024-05-23 16:17:36 -0300

    fix: resol. desafio2

--- src/Desafio2.java
+++ src/Desafio2.java
@@ -5,12 +5,11 @@ public class Desafio2 {
   public static void main(String[] args) {
     List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-    List<Integer> numerosPares = numeros.stream()
+    int numerosPares = numeros.stream()
       .filter(n -> n % 2 == 0)
-      .sorted()
-      .toList();
+      .reduce(0, Integer::sum);
 
-    numerosPares.forEach(System.out::println);
+    System.out.println(numerosPares);
   }
 
 }
---
+++ src/Desafio4.java
@@ -0,0 +1,6 @@
+public class Desafio4 {
+  public static void main(String[] args) {
+    
+  }
+
+}
