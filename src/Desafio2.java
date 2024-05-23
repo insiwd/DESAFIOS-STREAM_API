7fa31eeb7a641e955897844b47ccb1772bff6823
ruan <ruan.cavalcante505@gmail.com>
2024-05-23 04:11:00 -0300

    feat: Desafio2 - done

--- src/Desafio2.java
+++ src/Desafio2.java
@@ -1,6 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Desafio2 {
   public static void main(String[] args) {
-    System.out.println("hello world");
+    List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+    List<Integer> numerosPares = numeros.stream()
+      .filter(n -> n % 2 == 0)
+      .sorted()
+      .toList();
+
+    numerosPares.forEach(System.out::println);
   }
 
 }
