c18c09640a2db4d504cdfc57f8fc8c5788bef229
ruan <ruan.cavalcante505@gmail.com>
2024-05-24 10:56:33 -0300

    fix - doc

--- src/Desafio8.java
+++ src/Desafio8.java
@@ -3,6 +3,8 @@ import java.util.List;
 
 public class Desafio8 {
   public static void main(String[] args) {
+    // Desafio 8 - Somar os dígitos de todos os números da lista
+    
     List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
     Integer resultado = numeros.stream()
